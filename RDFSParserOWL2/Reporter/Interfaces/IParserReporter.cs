80cc912498daffccefd63e02db938a5f325993f7
aleksandar92 <aleksandar1992@hotmail.com>
2016-10-16 14:03:24 +0200

    Komitovan IParserReporter

---
+++ RDFSParserOWL2/Reporter/Interfaces/IParserReporter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDFSParserOWL2.Reporter.Interfaces
+{
+
+    public enum EntityTypesReporter
+    {
+        Class,
+        Property,
+        EnumMembers,
+        Unknown
+
+    }
+
+    public interface IParserReporter : IReporter
+    {
+
+        int EntityCount{get;}
+        int EntityCountByType(EntityTypesReporter type);
+        void AddtoEntityCountByType(EntityTypesReporter type, int number);
+        void RemoveFromEntityCountByType(EntityTypesReporter type, int number);
+    }
+}
